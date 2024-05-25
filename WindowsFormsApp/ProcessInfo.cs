f5fb6b27b1bed6117fecc6888316b6daad818e69
eaglclaws <eaglclaws@gmail.com>
2024-05-25 16:27:42 +0900

    class to consolidate process info

---
+++ WindowsFormsApp/ProcessInfo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Appointments.DataProvider;
+
+namespace WindowsFormsApp
+{
+    internal class ProcessInfo
+    {
+        private string _Name;
+        private string _Path;
+        private string _ExplorerPath;
+        public ProcessInfo(System.Diagnostics.Process p)
+        {
+            _Name = p.ProcessName;
+            _Path = p.MainModule.FileName;
+            _ExplorerPath = _Path;
+        }
+        public ProcessInfo(string path)
+        {
+            _Name = "explorer";
+            _Path = "explorer.exe";
+            _ExplorerPath = path;
+        }
+
+        public string Name { get { return _Name; } }
+        public string Path { get { return _Path; } }
+        public string ExplorerPath { get { return _ExplorerPath; } }
+    }
+}
