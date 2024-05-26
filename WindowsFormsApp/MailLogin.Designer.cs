bd5541817dfe31ab886f6284adf8508172a9a9e6
streetmeow <chojh106@gmail.com>
2024-05-26 16:06:27 +0900

    메일 로그인 기능

--- WindowsFormsApp/MailLogin.Designer.cs
+++ WindowsFormsApp/MailLogin.Designer.cs
@@ -28,10 +28,93 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            EmailLabel = new Label();
+            PasswordLabel = new Label();
+            EmailBox = new TextBox();
+            PasswordBox = new TextBox();
+            label1 = new Label();
+            EmailInsert = new Button();
+            SuspendLayout();
+            // 
+            // EmailLabel
+            // 
+            EmailLabel.AutoSize = true;
+            EmailLabel.Font = new Font("맑은 고딕", 11F);
+            EmailLabel.Location = new Point(13, 42);
+            EmailLabel.Name = "EmailLabel";
+            EmailLabel.Size = new Size(46, 20);
+            EmailLabel.TabIndex = 0;
+            EmailLabel.Text = "Email";
+            // 
+            // PasswordLabel
+            // 
+            PasswordLabel.AutoSize = true;
+            PasswordLabel.Font = new Font("맑은 고딕", 11F);
+            PasswordLabel.Location = new Point(13, 88);
+            PasswordLabel.Name = "PasswordLabel";
+            PasswordLabel.Size = new Size(32, 20);
+            PasswordLabel.TabIndex = 1;
+            PasswordLabel.Text = "PW";
+            // 
+            // EmailBox
+            // 
+            EmailBox.Location = new Point(65, 42);
+            EmailBox.Name = "EmailBox";
+            EmailBox.Size = new Size(228, 23);
+            EmailBox.TabIndex = 2;
+            // 
+            // PasswordBox
+            // 
+            PasswordBox.Location = new Point(65, 88);
+            PasswordBox.MaxLength = 32;
+            PasswordBox.Name = "PasswordBox";
+            PasswordBox.PasswordChar = '*';
+            PasswordBox.Size = new Size(228, 23);
+            PasswordBox.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("맑은 고딕", 14F, FontStyle.Bold);
+            label1.Location = new Point(87, 10);
+            label1.Name = "label1";
+            label1.Size = new Size(159, 25);
+            label1.TabIndex = 4;
+            label1.Text = "이메일 계정 정보";
+            // 
+            // EmailInsert
+            // 
+            EmailInsert.Font = new Font("맑은 고딕", 11F, FontStyle.Bold);
+            EmailInsert.Location = new Point(118, 117);
+            EmailInsert.Name = "EmailInsert";
+            EmailInsert.Size = new Size(108, 32);
+            EmailInsert.TabIndex = 5;
+            EmailInsert.Text = "정보 입력";
+            EmailInsert.UseVisualStyleBackColor = true;
+            // 
+            // MailLogin
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(EmailInsert);
+            Controls.Add(label1);
+            Controls.Add(PasswordBox);
+            Controls.Add(EmailBox);
+            Controls.Add(PasswordLabel);
+            Controls.Add(EmailLabel);
+            Name = "MailLogin";
+            Size = new Size(317, 164);
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label EmailLabel;
+        private Label PasswordLabel;
+        private TextBox EmailBox;
+        private TextBox PasswordBox;
+        private Label label1;
+        private Button EmailInsert;
     }
 }
